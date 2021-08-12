19de1caf251081f25f3afdece66c551d0f89e1a7
Jason Hargrove <rijaha@gmail.com>
2021-08-12 15:25:24 -0500

    Practicing first problem

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // ===== Javascript Cardio Challenges ===== 
 
 // ===== Select 2 Challenges =====
+
+// Log everyting.
+let log = console.log
+
+// ==================== 1. ====================
 console.log (`
 
  CHALLENGE 1: LONGEST WORD
@@ -8,56 +13,44 @@ console.log (`
 // Return the longest word of a string
 // ex. longestWord('Hi there, my name is Brad') === 'there,'
 
-// const findLongestWord = (sen) => {
-//     let senSplit = sen.split(' ');
-//     let longestWord = 0;
-//     for (let i = 0; i < senSplit.length; i++) {
-//         if(senSplit[i].length > longestWord) {
-//             longestWord = senSplit[i].length;
-//         }
-//     }
-//     return longestWord
-// };
-// console.log(findLongestWord('Hi there, my name is J')); // 6
-
-// const findLongestWord = (sen) => {
-//     let senSplit = sen.split(' ');
-//     let longestWord = senSplit.sort(function(a, b) {
-//         return b.length - a.length;
-//     });
-//     return longestWord[0]
-// };
-// console.log(findLongestWord('Hi there, my name is J')); // there,
-
 const longestWord = (sen) => {
-    // Create filtered array.
-    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g);
-    
-    //Sort by length.
-    const sorted = wordArr.sort((a, b) => b.length - a.length);
-     // SOLUTION 1 - Return a single longest word
+   
+    // Create filtered array using .match. Get rid of the "," and make it all lowercase.
+    const wordArr = sen.toLowerCase().match(/[a-z0-9]+/g)
+    const sorted = wordArr.sort(function(a,b) {
+        return b.length - a.length
+    })
+    const longestWordArr = sorted.filter(function(word) {
+        return word.length === sorted[0].length
+    })
+    if (longestWordArr.length === 1) {
+        return longestWordArr[0];
+    } else {
+        return longestWordArr
+    }
+
+    // SOLUTION 1 - Return a single longest word
+    //Sort by length. I want the longest word at the begining.
+  
     //  return sorted[0];
 
     // SOLUTION 2 - Return an array and include multiple words if they have the same length
     // If multiple words, put into array.
-    const longestWordArr = sorted.filter((word) => word.length === sorted[0].length);
+ 
      // Part of SOLUTION 2
     // console.log(longestWordArr)
 
     // SOLUTION 3 - Only return an array if multiple words, otherwise return a string
     // Check if more thatn one array val
-    if (longestWordArr.length === 1) {
-        // Return the word
-        return longestWordArr[0];
-    } else {
-        return longestWordArr;
-    }
+ 
+  
 }
-console.log(longestWord('Hello there, my name is Jason'))
+console.log(longestWord('Hi ther, my name is Jason'))
 // Testing SOLUTION 3
 // console.log(longestWord('Helloo there, my name is Jason'))
 
 
+// ==================== 2. ====================
 console.log (`
 
 CHALLENGE 2: ARRAY CHUNKING
@@ -67,11 +60,14 @@ CHALLENGE 2: ARRAY CHUNKING
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
 const chunkArray = (arr, len) => {
-
-
+   
 }
+log(chunkArray([1, 2, 3, 4, 5, 6, 7], 3)) // === [[1, 2, 3],[4, 5, 6],[7]])
+// log(chunkArray([1, 2, 3, 4, 5, 6, 7], 2) // === [[1, 2],[3, 4],[5, 6],[7]])
 
 
+
+// ==================== 3. ====================
 console.log (`
 
 CHALLENGE 3: FLATTEN ARRAY
@@ -82,6 +78,7 @@ CHALLENGE 3: FLATTEN ARRAY
 function flattenArray(arrays) {}
 
 
+// ==================== 4. ====================
 console.log (`
 
 CHALLENGE 4: ANAGRAM
@@ -93,6 +90,7 @@ CHALLENGE 4: ANAGRAM
 function isAnagram(str1, str2) {}
 
 
+// ==================== 5. ====================
 console.log (`
 
 CHALLENGE 5: LETTER CHANGES
