78f9a3dcf60a5c033f1fe141ae6d65397f6f80ee
Jason Hargrove <rijaha@gmail.com>
2021-08-11 21:21:49 -0500

    On to array chunking

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 // ===== Javascript Cardio Challenges ===== 
 
 // ===== Select 2 Challenges =====
+console.log (`
 
-// CHALLENGE 1: LONGEST WORD
+ CHALLENGE 1: LONGEST WORD
+`)
 // Return the longest word of a string
 // ex. longestWord('Hi there, my name is Brad') === 'there,'
 
@@ -55,28 +57,46 @@ console.log(longestWord('Hello there, my name is Jason'))
 // Testing SOLUTION 3
 // console.log(longestWord('Helloo there, my name is Jason'))
 
- 
-// ===== CHALLENGE 2: ARRAY CHUNKING =====
+
+console.log (`
+
+CHALLENGE 2: ARRAY CHUNKING
+`)
 // Split an array into chunked arrays of a specific length
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
 // ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
 
-function chunkArray(arr, len) {}
+const chunkArray = (arr, len) => {
 
-// ===== CHALLENGE 3: FLATTEN ARRAY =====
+
+}
+
+
+console.log (`
+
+CHALLENGE 3: FLATTEN ARRAY
+`)
 // Take an array of arrays and flatten to a single array
 // ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
 
 function flattenArray(arrays) {}
 
-// ===== CHALLENGE 4: ANAGRAM =====
+
+console.log (`
+
+CHALLENGE 4: ANAGRAM
+`)
 // Return true if anagram and false if not
 // ex. 'elbow' === 'below'
 // ex. 'Dormitory' === 'dirty room##'
 
 function isAnagram(str1, str2) {}
 
-// ===== CHALLENGE 5: LETTER CHANGES =====
+
+console.log (`
+
+CHALLENGE 5: LETTER CHANGES
+`)
 // Change every letter of the string to the one that follows it and capitalize the vowels
 // Z should turn to A
 // ex. 'hello there' === 'Ifmmp UIfsf'
